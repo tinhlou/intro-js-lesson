a7bbee49f00735a4e0999e8625f4d2e227200d47
quincyp <quincytpham@gmail.com>
2023-03-28 15:26:44 -0700

    Created basic html, css, js with js lessons to cover

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1. Link the script.js file from the HTML
+
+// 2. Console Log + Comments + Reserved Words + Camel Case + Naming Convention
+
+// 3. Creating Variables + Data Types
+
+// 4. Creating Functions (+ Event Listeners?)
+
+// 5. Strings
+
+// 6. Numbers
+
+// 7. Arrays and Loops
+
+// 8. If Statements/Conditionals
+
+// 9. DOM Manipulation
+
+// 10. Events
+
