d644b2cb13756c6fea2403f6303badf06d54a2d3
tinhlou <tinhlou1@gmail.com>
2023-03-28 16:59:34 -0700

    followed quincy

--- lecture.js
+++ lecture.js
@@ -5,17 +5,34 @@ Make sure to code-along during lecture.
 
 
 // 1. Link the script.js file from the HTML
+var myNumber = 3;
 
+    alert(myNumber);
+
+    myNumber = 8;
 // 2. Console Log + Comments + Reserved Words + Camel Case + Naming Convention
 
 // 3. Creating Variables + Data Types
-
+    let text = "this is a sentence";
+    //Placing the variable into a function()
+    alert(text);
+    console.log(text);
 // 4. Creating Functions (+ Event Listeners?)
 
-// 5. Strings
-
-// 6. Numbers
-
+// 5. Strings = text
+    
+// 6. Numbers = number,
+    /* big int is a big integer 
+    boolean = true or false
+    undefined = value is undefined until you set one
+    null = no value
+    */
+    //3 object data types
+    /*
+    object = list of key:value pairs
+    array =indexed list of values
+    date = date
+    */
 // 7. Arrays and Loops
 
 // 8. If Statements/Conditionals
