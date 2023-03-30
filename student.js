e74ed55841d41db425e57fa17c6a346f22f09818
tinhlou <tinhlou1@gmail.com>
2023-03-30 16:12:18 -0700

    followed along

--- student.js
+++ student.js
@@ -4,14 +4,19 @@ Complete the following numbered tasks:
 */
 
 // 1. Declare 2 variables below:
-
+    let fruit;
+    let apple;
 
 // Initialize 3 variables:
 
     // 2. Assign one to the type Number below:
-
+    fruit = 2;
 
     // 3. Assign one to the type Boolean below:
-    
+    apple = true;
 
     // 4. Assign one to the type String below:
+    apple = "Orange";
+
+console.log(fruit)
+console.log(apple)
